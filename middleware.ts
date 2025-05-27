00dfa8c6d9d985a3ae21005c1cc518d867174b0c
Adeun <93503031+Adeun-Ilemobola@users.noreply.github.com>
2025-05-26 21:22:32 -0700

    temp rename login route to temp-login

--- middleware.ts
+++ middleware.ts
@@ -2,32 +2,28 @@ import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
 const Rount = {
-  auth: ["/login", "/register" ,"/"],
+  auth: ["/login", "/register", "/"],
   main: ["/home"],
 };
 
 export async function middleware(request: NextRequest) {
   const pathName = request.nextUrl.pathname;
   console.log("------- middleware -------");
- 
+
   const isAuthentication = Rount.auth.some((route) => pathName.startsWith(route));
   const isMain = Rount.main.some((route) => pathName.startsWith(route));
+  const sessionCookie = getSessionCookie(request);
 
-   const sessionCookie = getSessionCookie(request);
-
-   console.log({
+  console.log({
     isAuthentication,
     isMain,
     sessionCookie,
     pathName
 
-   });
-    if (request.nextUrl.pathname.startsWith('/api/auth')) {
+  });
+  if (request.nextUrl.pathname.startsWith('/api/auth')) {
     return NextResponse.next();
   }
-   
-
-
   if (!sessionCookie && isMain && pathName !== "/login") {
     return NextResponse.redirect(new URL("/login", request.url));
   }
