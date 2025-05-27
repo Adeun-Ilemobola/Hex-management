52e5ac61698a1d79ddcc945f042e8f1045ed0584
Adeun <93503031+Adeun-Ilemobola@users.noreply.github.com>
2025-05-26 21:50:19 -0700

    feat: remove unused experimental options from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  swcMinify: true, // Faster builds
   experimental: {
     serverActions: {
     bodySizeLimit: '7mb', // or whatever value you want
     allowedOrigins: ['https://hex-management.vercel.app/'],
   }, // Unlocks server actions (Next.js 14+)
-    nodeMiddleware: true, // For custom node middleware (as needed)
-    // turbo mode is not stable yet, but keep an eye on it!
-    // turbo: true, 
-    // For edge runtime, see notes below
+  
   },
   images: {
     domains: ['your-supabase-bucket-url', 'cdn.shadcn.com'],
