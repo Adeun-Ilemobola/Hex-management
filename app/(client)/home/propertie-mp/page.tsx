76d8bb25e56f5cab62f1a9a578dcf8bdbb0d52bd
Adeun <93503031+Adeun-Ilemobola@users.noreply.github.com>
2025-08-03 01:30:53 -0700

    wrapped  a suspense boundary at page "/home/propertie-mp"

--- app/(client)/home/propertie-mp/page.tsx
+++ app/(client)/home/propertie-mp/page.tsx
@@ -1,10 +1,16 @@
 import PropertyModification from "@/components/(propertyFragments)/PropertyModification";
+import Loading from "@/components/Loading";
+import { Suspense } from "react";
 
 export default async function Page() {
    
     return (
         <div className='relative flex flex-col  min-h-screen  overflow-hidden '>
-            <PropertyModification />
+
+            <Suspense fallback={<Loading />}>
+                        <PropertyModification />
+
+            </Suspense>
         </div>
     )
 }
