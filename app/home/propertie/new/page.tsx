4950f0f4d06cffc9a2b719b287de64c71a8e76a7
“Adeun” <adeunilemobola@gmail.com>
2025-12-30 23:51:18 -0800

    fix: wrap MultiStepForm in Suspense to handle loading state

--- app/home/propertie/new/page.tsx
+++ app/home/propertie/new/page.tsx
@@ -1,9 +1,14 @@
+import Loading from '@/components/Loading'
 import MultiStepForm from '@/components/Property/MultiStepForm'
+import { Suspense } from 'react'
 
 export default function page() {
   return (
     <>
+    <Suspense fallback={<Loading />}>
     <MultiStepForm />
+    </Suspense>
+    
         
     </>
   )
